3634ab108d80f3b4b3c8fc755d98256bb00b19aa
juunaaa <juunaaa1203@gmail.com>
2022-07-28 16:11:17 +0900

    v1

---
+++ Q3.py
@@ -0,0 +1,13 @@
+#Q3
+def solution(left, right):
+    answer = 0
+    for i in range(left, right+1):
+        if (i**(1/2)).is_integer():
+            answer -= i
+        else:
+            answer += i
+    return answer
+
+left, right = map(int, input().split())
+c = solution(left, right)
+print(c)
